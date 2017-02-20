d1604c4601e5fecf6d233417604e34929cf24126
chrisreis53 <chrisreis53@gmail.com>
2017-02-20 13:21:47 -0700

    Removed whitespace

--- src/KafkaConnection.cpp
+++ src/KafkaConnection.cpp
@@ -1,15 +1,9 @@
-
 /*
  * Copyright 2007-2016 United States Government as represented by the
  * Administrator of The National Aeronautics and Space Administration.
  * No copyright is claimed in the United States under Title 17, U.S. Code.
  * All Rights Reserved.
  */
-
-
-
-
-
 
 
 /* @file KafkaConnection.cpp
